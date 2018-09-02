2c85ff4909d6fbff6263dda7d9f13e843c436934
jeremyfa <contact@jeremyfa.com>
2018-09-02 23:39:08 +0200

    Update to latest haxe grammar

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 cd "${0%/*}"
 
 haxelib run vshaxe-build --target tm-language
-cp -f ./haxe.tmLanguage ~/.vscode/extensions/nadako.vshaxe-1.11.0/syntaxes/haxe.tmLanguage
-cp -f ./haxe.YAML-tmLanguage ~/.vscode/extensions/nadako.vshaxe-1.11.0/syntaxes/haxe.YAML-tmLanguage
+cp -f ./haxe.tmLanguage ~/.vscode/extensions/nadako.vshaxe-2.4.0/syntaxes/haxe.tmLanguage
+cp -f ./haxe.YAML-tmLanguage ~/.vscode/extensions/nadako.vshaxe-2.4.0/syntaxes/haxe.YAML-tmLanguage
